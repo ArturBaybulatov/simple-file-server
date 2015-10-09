f56316c5c69c6fc9d2a3ea367169ebdf2a13b824
Nieralyte <NieralyteCommon@Gmail.com>
2015-10-09 22:18:55 +0500

    simplified

--- index.js
+++ index.js
@@ -7,18 +7,11 @@ var http = require('http')
 var serveStatic = require('serve-static')
 var serveIndex = require('serve-index')
 var morgan = require('morgan') // logger
-var path = require('path')
-
-var PATH = path.resolve(
-  process.argv[2] || process.env.SERVER_PATH || '.'
-)
-
-var PORT = process.env.PORT || 80
 
 var app = connect()
 
 app.use(morgan('dev'))
-app.use(serveStatic(PATH))
-app.use(serveIndex(PATH))
+app.use(serveStatic(path.resolve('.')))
+app.use(serveIndex(path.resolve('.')))
 
-http.createServer(app).listen(PORT)
+http.createServer(app).listen(80)
