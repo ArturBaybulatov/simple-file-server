c37e4d46b0ddb18e9ac911e3f75dc60631aa867f
Nieralyte <NieralyteCommon@Gmail.com>
2015-10-11 15:01:45 +0500

    simplify things

---
+++ README.md
@@ -0,0 +1,18 @@
+# simple-file-server
+simple file server done using connect middleware
+
+### usage
+
+install globally with:
+
+```
+npm install https://github.com/Nieralyte/simple-file-server --global
+```
+
+then in command line type:
+
+```
+cd some/target/folder && simple-file-server
+```
+
+a simple web server will start at `localhost:80` serving files from `some/target/folder`
--- index.js
+++ index.js
@@ -3,16 +3,12 @@
 'use strict'
 
 var connect = require('connect')
-var http = require('http')
 var serveStatic = require('serve-static')
 var serveIndex = require('serve-index')
 var morgan = require('morgan') // logger
-var path = require('path')
 
-var app = connect()
-
-app.use(morgan('dev'))
-app.use(serveStatic(path.resolve('.')))
-app.use(serveIndex(path.resolve('.')))
-
-http.createServer(app).listen(80)
+connect()
+  .use(morgan('dev'))
+  .use(serveStatic('.'))
+  .use(serveIndex('.'))
+  .listen(80)
